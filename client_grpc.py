f81e84c31664a8df91ae67483a789b1b9fb198a5
Joshua Michels <joshuamichels@dhcp-10-250-226-222.harvard.edu>
2023-04-21 21:14:57 -0400

    created open new file function, issues with importing helpers file in server

--- client_grpc.py
+++ client_grpc.py
@@ -6,102 +6,41 @@ import time
 import grpc
 import texteditor_pb2
 import texteditor_pb2_grpc
+
 # import helpers_grpc
 
-# Loops until a valid sign in. Returns username of signed in user.
-def signinLoop(stub):
-    existsBool = helpers_grpc.existingOrNew()
-    # Try to sign in existing user
-    if existsBool:
-        print("Please log in with your username")
-        username = input("Username: ")
-        # Username error check
-        if helpers_grpc.isValidUsername(username):
-            # Remove whitespace
-            username = username.strip().lower()
-            unreadsOrError = stub.SignInExisting(texteditor_pb2.Username(name=username))
-            eFlag, msg = unreadsOrError.errorFlag, unreadsOrError.unreads
-    # Try to sign in new user
-    else:
-        print("\nPlease create a new username.")
-        username = input("New Username: ")
-        # Username error check
-        if helpers_grpc.isValidUsername(username):
-            # Remove whitespace
-            username = username.strip().lower()
-            unreadsOrError = stub.AddUser(texteditor_pb2.Username(name=username))
-            eFlag, msg = unreadsOrError.errorFlag, unreadsOrError.unreads
-    if eFlag:
-        print(msg)
-        return signinLoop(stub)
-    else:
-        print("\nCongratulations! You have successfully logged in to your account.\n")
-        print(msg)
-        return username
-
 # Parse input from command line and do the correct action. Loops until logout or delete.
-def messageLoop(username, stub):
+def commandLoop(stub):
     command = sys.stdin.readline().strip()
     # User wants to send a message
-    if command == 'S' or command == 's':
+    if command == 'O' or command == 'o':
+        new_filename = ""
         while True:
-            send_to_user = input("Which user do you want to message? \n Recipient username: ")
-            # Username error checks
-            if not helpers_grpc.isValidUsername(send_to_user):
-                continue
-            if send_to_user == username: 
-                print("Cannot send message to self.\n")
-                continue
+            new_filename = input("Enter a name for the new file. \n Filename: ")
+            # Insert check if filename is valid here?
             break
-        message = input("Type the message you would like to send. \n Message: ")
         # Send sender username, recipient username, and message to the server & store confirmation response
-        sender = texteditor_pb2.Username(name=username)
-        recipient = texteditor_pb2.Username(name=send_to_user)
-        payload = texteditor_pb2.Payload(msg=message)
-        senderResponse = stub.Send(texteditor_pb2.SendRequest(sender=sender, recipient=recipient, sentMsg=payload))
+        senderResponse = stub.OpenNewFile(texteditor_pb2.Download(filename=new_filename))
         print(senderResponse.msg)
         print("Command:")
-    # User wants to list users
-    if command == 'L' or command == 'l':
-        wildcard = input("Optional text wildcard: ")
-        if wildcard == "":
-            wildcard = "*"
-        listResponse = stub.List(texteditor_pb2.Payload(msg=wildcard))
-        print("Fetching users... \n")
-        print(listResponse.msg)
-        print("Command:")
-    # User wants to log out
-    if command == 'O' or command == 'o':
-        logoutResponse = stub.Logout(texteditor_pb2.Username(name=username))
-        print("Logging out...")
-        time.sleep(0.2)
-        print(logoutResponse.msg)
-        time.sleep(0.2)
-        return
-    # User wants to delete account
-    if command == 'D' or command == 'd':
-        confirm = False
-        confirmInput = input("Are you sure? Deleted accounts are permanently erased, "
-                            "and you will be logged off immediately. [Y/N] ")
-        while True:
-            if confirmInput == 'Y' or confirmInput == 'y':
-                confirm = True
-                break
-            elif confirmInput == 'N' or confirmInput == 'n':
-                confirm = False
-                break
-            else:
-                print("Invalid response. Please answer with 'Y' or 'N'.")
-        if confirm:
-            deleteResponse = stub.Delete(texteditor_pb2.Username(name=username))
-            print("Deleting account... \n")
-            time.sleep(0.2)
-            print(deleteResponse.msg)
-            time.sleep(0.2)
-            return
-        else:
-            print("\nCommand: ")
-    messageLoop(username, stub)
+    # # User wants to list users
+    # if command == 'L' or command == 'l':
+    #     wildcard = input("Optional text wildcard: ")
+    #     if wildcard == "":
+    #         wildcard = "*"
+    #     listResponse = stub.List(texteditor_pb2.Payload(msg=wildcard))
+    #     print("Fetching users... \n")
+    #     print(listResponse.msg)
+    #     print("Command:")
+    # # User wants to log out
+    # if command == 'O' or command == 'o':
+    #     logoutResponse = stub.Logout(texteditor_pb2.Username(name=username))
+    #     print("Logging out...")
+    #     time.sleep(0.2)
+    #     print(logoutResponse.msg)
+    #     time.sleep(0.2)
+    #     return
+    commandLoop(stub)
 
 # Listens for messages from server's Listen response stream. Closes when user logs out or deletes acct.
 def listen_thread(username, stub, responseStream):
@@ -113,30 +52,26 @@ def listen_thread(username, stub, responseStream):
             return
 
 def run():
-    ip = '10.250.31.89'
+    ip = '10.250.226.222'
     port = '8080'
     with grpc.insecure_channel('{}:{}'.format(ip, port)) as channel:
-        stub = texteditor_pb2_grpc.ChatStub(channel)
+        stub = texteditor_pb2_grpc.TextEditorStub(channel)
         print("Congratulations! You have connected to the chat server.\n")
 
         while True:
-            username = signinLoop(stub)
             # Now, the user is logged in. Notify the user of possible functions
             print("If any messages arrive while you are logged in, they will be immediately displayed.\n")
             print("Use the following commands to interact with the chat app: \n")
             print(" -----------------------------------------------")
-            print("|L: List all accounts that exist on this server.|")
-            print("|S: Send a message to another user.             |")
-            print("|O: Log Out.                                    |")
-            print("|D: Delete account.                             |")
+            print("|L: List all files that exist on this server.   |")
+            print("|O: Open a new file.                            |")
+            print("|E: Open an existing file.                      |")
             print(" ----------------------------------------------- \n")
             print("Command: ")
             # Establish response stream to receive messages from server.
             # responseStream is a generator of chat_pb2.Payload objects.
-            responseStream = stub.Listen(texteditor_pb2.Username(name=username))
-            start_new_thread(listen_thread, (username, stub, responseStream))
             # Wait for input from command line
-            messageLoop(username, stub)
+            commandLoop(stub)
 
 
 if __name__ == '__main__':
