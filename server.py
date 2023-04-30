ce822245156d534a500495c09f5c261667b98f8f
James Barna <jamesbarna@college.harvard.edu>
2023-04-30 13:40:19 -0400

    richer GUI

---
+++ server.py
@@ -0,0 +1,91 @@
+from concurrent import futures
+import logging
+
+import grpc
+import sys
+import texteditor_pb2
+import texteditor_pb2_grpc
+import helpers
+
+
+class TextEditorServicer(texteditor_pb2_grpc.TextEditorServicer):
+
+    def __init__(self):
+        # set of (unique) filenames
+        self.filenames = set()
+    
+    def OpenNewFile(self, download, context):
+        if helpers.filenameExists(download.filename, self.filenames):
+            return texteditor_pb2.FileResponse(errorFlag=True, filename=download.filename)
+        self.filenames.add(download.filename)
+        open("./usertextfiles/" + download.filename + ".txt", "w")
+        return texteditor_pb2.FileResponse(errorFlag=False, filename=download.filename)
+    
+    def OpenExistingFile(self, download, context):
+        print(download.filename)
+        # return error if file does not exist
+        try:
+            with open("./usertextfiles/" + download.filename + ".txt", "rb") as f:
+                while True:
+                    chunk = f.read(4096)
+                    if not chunk:
+                        break
+                    yield texteditor_pb2.FileContents(contents=chunk)
+        except IOError:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("File not found")
+            return
+
+
+#     def SignInExisting(self, username, context):
+#         eFlag, msg = helpers_grpc.signInExisting(username.name, self.clientDict)
+#         return texteditor_pb2.Unreads(errorFlag=eFlag, unreads=msg)
+    
+#     def AddUser(self, username, context):
+#         eFlag, msg = helpers_grpc.addUser(username.name, self.clientDict)
+#         return texteditor_pb2.Unreads(errorFlag=eFlag, unreads=msg)
+
+#     def Send(self, sendRequest, context):
+#         response = helpers_grpc.sendMsg(sendRequest.sender.name, sendRequest.recipient.name, 
+#                                         sendRequest.sentMsg.msg, self.clientDict)
+#         return texteditor_pb2.Payload(msg=response)
+
+#     # usernameStream only comes from logged-in user
+#     def Listen(self, username, context):
+#         while True:
+#             # If any messages are queued
+#             if len(self.clientDict[username.name][1]) > 0:
+#                 # Yield first message
+#                 yield texteditor_pb2.Payload(msg=self.clientDict[username.name][1].pop(0))
+#             # Stop stream if user logs out
+#             if self.clientDict[username.name][0] == False:
+#                 break
+
+#     def List(self, wildcard, context):
+#         payload = helpers_grpc.sendUserlist(wildcard.msg, self.clientDict)
+#         return texteditor_pb2.Payload(msg=payload)
+
+#     def Logout(self, username, context):
+#         self.clientDict[username.name][0] = False
+#         return texteditor_pb2.Payload(msg="Goodbye!\n")
+
+#     def Delete(self, username, context):
+#         self.clientDict.pop(username.name)
+#         return texteditor_pb2.Payload(msg="Goodbye!\n")
+
+
+def serve():
+    # ip = '10.250.226.222'
+    ip = '10.250.64.41'
+    port = '8080'
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    texteditor_pb2_grpc.add_TextEditorServicer_to_server(TextEditorServicer(), server)
+    server.add_insecure_port(f"{ip}:{port}")
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
---
+++ tk_sandbox.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+"""Starter code source: realpython.com/python-gui-tkinter/#building-a-text-editor-example-app"""
+
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Distributed Collaborative Text Editor - {filepath}")
+
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, mode="w", encoding="utf-8") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+window = tk.Tk()
+window.title("Distributed Collaborative Text Editor - New File")
+
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = tk.Text(window)
+frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
+btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
+btn_quit = tk.Button(frm_buttons, text="Quit", command=window.destroy)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+btn_quit.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+
+frm_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
