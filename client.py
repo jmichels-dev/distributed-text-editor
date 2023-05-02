1162dc75df650c3a64d434feb0615d0acb6d27f5
James Barna <jamesbarna@college.harvard.edu>
2023-05-02 10:43:35 -0400

    Correct gui behavior

--- client.py
+++ client.py
@@ -11,108 +11,7 @@ import grpc
 import texteditor_pb2
 import texteditor_pb2_grpc
 
-import helpers
-
-filetypes = [("Text Files", "*.txt")]
-
-# Parse input from command line and do the correct action. Loops until logout or delete.
-def commandLoop(stub):
-    command = sys.stdin.readline().strip()
-    # User wants to open new .txt file
-    if command == 'O' or command == 'o':
-        new_filename = ""
-        while True:
-            new_filename = input('''Enter a name for the new file (.txt file extension will be automatically added). \n
-                                 Filename: ''')
-            # Insert check if filename is valid here?
-            break
-        # Send sender username, recipient username, and message to the server & store confirmation response
-        fileResponse = stub.OpenNewFile(texteditor_pb2.Download(filename=new_filename))
-        if fileResponse.errorFlag:
-            print("Error: Filename " + fileResponse.download + ".txt already exists.")
-        else:
-            # Client downloads local copy of new file from server and launches editor
-            launchGUI(fileResponse.filename)
-
-    # User wants to open existing.txt file
-    if command == 'E' or command == 'e':
-        existing_filename = ""
-        while True:
-            existing_filename = input("Enter existing file name. (omit file type) \n Filename: ")
-            # Insert check if filename is valid here?
-            break
-        # Send sender username, recipient username, and message to the server & store confirmation response
-        fileResponse = stub.OpenExistingFile(texteditor_pb2.Download(filename=existing_filename))
-        # if fileResponse.errorFlag:
-        #     print("Error: Filename already exists.")
-        # else:
-        with open(existing_filename + ".txt", "wb") as f:
-            for response in fileResponse:
-                f.write(response.contents)
-        launchGUI(fileResponse.filename)
-
-    # # User wants to list users
-    # if command == 'L' or command == 'l':
-    #     wildcard = input("Optional text wildcard: ")
-    #     if wildcard == "":
-    #         wildcard = "*"
-    #     listResponse = stub.List(texteditor_pb2.Payload(msg=wildcard))
-    #     print("Fetching users... \n")
-    #     print(listResponse.msg)
-    #     print("Command:")
-    # # User wants to log out
-    # if command == 'O' or command == 'o':
-    #     logoutResponse = stub.Logout(texteditor_pb2.Username(name=username))
-    #     print("Logging out...")
-    #     time.sleep(0.2)
-    #     print(logoutResponse.msg)
-    #     time.sleep(0.2)
-    #     return
-    commandLoop(stub)
-
-def launchGUI(filename):
-    appName = "Distributed Text Editor"
-    
-    window = Tk()
-    window.title(appName + " - " + filename)
-    window.geometry('500x400')
-    window.grid_columnconfigure(0, weight=1)
-
-    txt = scrolledtext.ScrolledText(window, height=999)
-    txt.grid(row=1,sticky=tk.N+tk.S+tk.E+tk.W)
-    #txt.bind("<KeyPress>", textchange(window, appName, ))
-
-    menu = tk.Menu(window)
-    fileDropdown = tk.Menu(menu, tearoff=False)
-    fileDropdown.add_command(label="Open", command=lambda: fileDropdownHandler("open", filename, window, appName, txt))
-    fileDropdown.add_separator()
-    fileDropdown.add_command(label="Save", command=lambda: fileDropdownHandler("save", filename, window, appName, txt))
-    fileDropdown.add_command(label="Save as", command=lambda: fileDropdownHandler("saveAs", filename, window, appName, txt))
-    menu.add_cascade(label="File", menu=fileDropdown)
-
-    window.config(menu=menu)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    window.mainloop()
-
-# prolly will get rid of this
-def textchange(window, appName, event, filename):
-    window.title(appName + " -*" + filename)
-
-def fileDropdownHandler(action, filename, window, appName, txt):
-    if action == "open":
-        file = filedialog.askopenfilename(filetypes=filetypes)
-        window.title(appName + " - " + filename)
-        currentFilePath = file
-        with open(file, 'r') as f:
-            txt.delete(1.0, tk.END)
-            txt.insert(tk.INSERT,f.read())
-    elif action == "save" or action == "saveAs":
-        if currentFilePath == "New File" or action == "saveAs":
-            currentFilePath = filedialog.asksaveasfilename(filetypes=filetypes)
-        with open(currentFilePath, "w") as f:
-            f.write(txt.get("1.0", "end"))
-        window.title(appName + " - " + currentFilePath)
-        
+import helpers        
 
 # Listens for messages from server's Listen response stream. Closes when user logs out or deletes acct.
 def listen_thread(username, stub, responseStream):
@@ -130,13 +29,14 @@ def run():
     with grpc.insecure_channel('{}:{}'.format(ip, port)) as channel:
         stub = texteditor_pb2_grpc.TextEditorStub(channel)
         print("Congratulations! You have connected to the collaborative file editing server.\n")
-        gui = EditorGUI()
+        
+        editor = EditorGUI(stub)
 
-        while True:
+        print("is this blocking?")
+        # while True:
             # Establish response stream to receive messages from server.
             # responseStream is a generator of texteditor_pb2.??? objects.
             # Wait for input from command line
-            commandLoop(stub)
 
 
 if __name__ == '__main__':
--- tk_sandbox.py
+++ tk_sandbox.py
@@ -1,11 +1,16 @@
+import os
+
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import messagebox
 
 """Starter code source: realpython.com/python-gui-tkinter/#building-a-text-editor-example-app"""
 
 class EditorGUI():
-    def __init__(self):
+    def __init__(self, stub):
         """Run text editor GUI and save key attributes"""
+        self.stub = stub
+        
         self.window = tk.Tk()
         self.window.title("Distributed Collaborative Text Editor - New File")
         self.title = "Distributed Collaborative Text Editor - New File"
@@ -13,22 +18,38 @@ class EditorGUI():
         self.window.rowconfigure(0, minsize=800, weight=1)
         self.window.columnconfigure(1, minsize=800, weight=1)
         
-        self.txt_edit = tk.Text(self.window)
+        self.txt_edit = tk.Text(self.window, font=("Helvetica", 16))
         frm_buttons = tk.Frame(self.window, relief=tk.RAISED, bd=2)
-        btn_open = tk.Button(frm_buttons, text="Open", command=self.open_file)
+        btn_new = tk.Button(frm_buttons, text="New File", command=self.new_file)
+        btn_open = tk.Button(frm_buttons, text="Open...", command=self.open_file)
         btn_save = tk.Button(frm_buttons, text="Save", command=self.save)
         btn_saveas = tk.Button(frm_buttons, text="Save As...", command=self.save_as)
-        btn_quit = tk.Button(frm_buttons, text="Quit", command=self.window.destroy)
+        self.btn_delete = tk.Button(frm_buttons, text="Delete", command=self.delete, state="disabled", disabledforeground="black")
 
-        btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-        btn_saveas.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-        btn_quit.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+        btn_new.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        btn_open.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+        btn_save.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        btn_saveas.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_delete.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
 
         frm_buttons.grid(row=0, column=0, sticky="ns")
         self.txt_edit.grid(row=0, column=1, sticky="nsew")
 
         self.window.mainloop()
+
+    def new_file(self):
+        """Create a new file"""
+        # If there is text in the current file, prompt the user to save first
+        if self.txt_edit.get("1.0", tk.END).strip() != "":
+            response = messagebox.askyesnocancel("Save File", "Do you want to save your changes before creating a new file?")
+            if response == True:
+                # save the current file
+                self.save()
+
+        self.txt_edit.delete("1.0", tk.END)
+        self.window.title("Distributed Collaborative Text Editor - New File")
+        self.title = "Distributed Collaborative Text Editor - New File"
+        self.new_file_flag = True
 
     def open_file(self):
         """Open a file for editing."""
@@ -38,12 +59,21 @@ class EditorGUI():
         )
         if not filepath:
             return
+
+        if self.txt_edit.get("1.0", tk.END).strip() != "":
+            response = messagebox.askyesnocancel("Save File", "Do you want to save your changes before creating a new file?")
+            if response == True:
+                # save the current file
+                self.save()
+        
         self.txt_edit.delete("1.0", tk.END)
         with open(filepath, mode="r", encoding="utf-8") as input_file:
             text = input_file.read()
             self.txt_edit.insert(tk.END, text)
         self.window.title(f"Distributed Collaborative Text Editor - {filepath}")
         self.title = f"Distributed Collaborative Text Editor - {filepath}"
+        self.new_file_flag = False
+        self.btn_delete.config(state="normal")
 
     def save(self):
         """If new file, save as, otherwise save with existing title"""
@@ -60,6 +90,7 @@ class EditorGUI():
                 output_file.write(text)
             self.window.title(f"Distributed Collaborative Text Editor - {filepath}")
             self.title = f"Distributed Collaborative Text Editor - {filepath}"
+            messagebox.showinfo("File Saved", "Your file has been saved.")
 
     def save_as(self):
         """Save the current file as a new file"""
@@ -88,3 +119,23 @@ class EditorGUI():
         self.window.title(f"Distributed Collaborative Text Editor - {filepath}")
         self.title = f"Distributed Collaborative Text Editor - {filepath}"
         self.new_file_flag = False
+        self.btn_delete.config(state="normal")
+
+    def delete(self):
+        """Delete the current file"""
+        title_arr = self.title.split(" - ")
+        filepath = title_arr[1]
+        if not filepath:
+            return
+        response = messagebox.askyesnocancel("Delete file", "Do you want to delete this file?")
+        if response == True:
+            try:
+                os.remove(filepath)
+                self.txt_edit.delete('1.0', tk.END)
+                self.btn_delete.config(state="disabled")
+                messagebox.showinfo("File Deleted", "The file has been deleted.")
+                self.window.title("Distributed Collaborative Text Editor - New File")
+                self.title = "Distributed Collaborative Text Editor - New File"
+                self.new_file_flag = True
+            except OSError:
+                messagebox.showerror("Error", "The file could not be deleted.")
